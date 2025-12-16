e64ffbc42c78e00f2f1bdc874a2182d5a80f274a
Jeppe Krogh <kjej@aarhus.dk>
2025-12-16 11:32:57 +0100

    Exclude users where source = api

--- Repositories/ApiDataRepository.php
+++ Repositories/ApiDataRepository.php
@@ -84,6 +84,7 @@ class ApiDataRepository
             ->from("zp_user", "worker")
             ->select(["worker.id", "worker.username", DB::raw("CONCAT(worker.firstname, ' ', worker.lastname) as name")])
             ->where("worker.id", ">=", $startId)
+            ->where("worker.source", "<>", "api")
             ->when($modifiedAfter !== null, fn ($query) => $query->where("worker.modified", ">=", CarbonImmutable::createFromTimestamp($modifiedAfter)->format(APIData::DATE_FORMAT)))
             ->when($ids !== null, fn ($query) => $query->whereIn("worker.id", $ids))
             ->orderBy("worker.id", "ASC")
